e9a102d80093fa5ab18db8f4cb25037dd1ea07e5
João Job <joao.gabrielfjob@gmail.com>
2024-04-15 16:22:49 -0300

    fix: add const to declare arrow function

--- api.js
+++ api.js
@@ -1,16 +1,21 @@
-minify = async (url) => {
+const minify = async (url) => {
   const request = { url }
 
-  const response = await fetch(
-    'https://miniurl.api.joaojob.dev',
-    {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(request)
-    }
-  )
-
-  return await response.json()
+  try {
+    const response = await fetch(
+      'https://miniurl.api.joaojob.dev',
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(request)
+      }
+    )
+  
+    return await response.json()
+  } catch(expection) {
+    console.error(expection)
+    return { }
+  }
 }
 
 export default minify
